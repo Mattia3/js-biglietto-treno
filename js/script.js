7708525f039e5653ec4d7031b73b7c622d264657
Mattia3 <mcortinovis9@gmail.com>
2021-10-08 15:37:26 +0200

    modificato

--- js/script.js
+++ js/script.js
@@ -6,18 +6,24 @@ const etaDelPasseggero = prompt("Inserire la propria età");
 console.log("Chilometri che vogliono percorrere", nChilometri);
 console.log("età del passeggero", etaDelPasseggero);
 
-
-const prezzoBiglietto = Math.min(nChilometri * 0.21);
+let prezzoBiglietto = nChilometri * 0.21;
 console.log("Prezzo del biglietto", prezzoBiglietto);
+prezzoBiglietto = prezzoBiglietto.toFixed(2)
 
-const scontoMinori = Math.min(prezzoBiglietto / 100 * 20)
-Math.min(scontoMinori - prezzoBiglietto)  
+let scontoMinori = prezzoBiglietto / 100 * 20;
+scontoMinori = scontoMinori.toFixed(2)
 console.log("Sconto minori", scontoMinori)
 
-const scontoOver = Math.min(prezzoBiglietto / 100 * 40)
-Math.min(scontoOver - prezzoBiglietto)  
+let scontoOver = prezzoBiglietto / 100 * 40
+scontoOver = scontoOver.toFixed(2)
 console.log("Sconto Over 65", scontoOver)
 
+
 if(etaDelPasseggero <= 17){
-  
+  alert("Hai diritto ad uno sconto del 20%")
+  alert(prezzoBiglietto - scontoMinori)
+} else if(etaDelPasseggero >=65){
+  alert("Hai diritto ad uno sconto del 40%")
+  alert(prezzoBiglietto - scontoOver)
 }
+
