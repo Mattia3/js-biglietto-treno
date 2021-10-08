b2c5e4af3e3fb7dee4e6e3dc5bff5ab8498a7d0e
Mattia3 <mcortinovis9@gmail.com>
2021-10-08 16:53:52 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
 console.log("Esercizio Biglietti treno")
 
-let nChilometri = prompt("Inserire i chilometri che si desidera percorrere");
-const etaDelPasseggero = prompt("Inserire la propria età");
+const nChilometri = parseInt(prompt("Inserire i chilometri che si desidera percorrere"));
+if(isNaN(nChilometri)){
+  alert("Testo non valido")
+}
+const etaDelPasseggero = parseInt(prompt("Inserire la propria età"));
+if(isNaN(etaDelPasseggero)){
+  alert("Testo non valido")
+}
 
 console.log("Chilometri che vogliono percorrere", nChilometri);
 console.log("età del passeggero", etaDelPasseggero);
 
 let prezzoBiglietto = nChilometri * 0.21;
 console.log("Prezzo del biglietto", prezzoBiglietto);
-prezzoBiglietto = prezzoBiglietto.toFixed(2)
 
 let scontoMinori = prezzoBiglietto / 100 * 20;
-scontoMinori = scontoMinori.toFixed(2)
 console.log("Sconto minori", scontoMinori)
 
 let scontoOver = prezzoBiglietto / 100 * 40
-scontoOver = scontoOver.toFixed(2)
 console.log("Sconto Over 65", scontoOver)
 
-
+let prezzoTotale;
+let prezzoTotaleOver;
 if(etaDelPasseggero <= 17){
+  prezzoTotale = prezzoBiglietto - scontoMinori
   alert("Hai diritto ad uno sconto del 20%")
-  alert(prezzoBiglietto - scontoMinori)
+  alert(prezzoTotale.toFixed(2))
+  
 } else if(etaDelPasseggero >=65){
+  prezzoTotaleOver = prezzoBiglietto - scontoOver
   alert("Hai diritto ad uno sconto del 40%")
-  alert(prezzoBiglietto - scontoOver)
+  alert(prezzoTotaleOver.toFixed(2))
+} else{
+  alert(prezzoBiglietto)
 }
+
+
+
 
